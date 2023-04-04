32461963cc6f91208acbd2ff502d75ac47797237
kavinda <kavinda.sandaruwan32@gmail.com>
2023-04-05 00:05:28 +0530

    user update function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -33,7 +33,7 @@ function App() {
           <Route exact path="/Admin/AddUser" element={<AddUser/>}/>
           <Route exact path="/Admin/Jobs" element={<Jobs/>}/>
           <Route exact path="/Admin/AddJobs" element={<AddJobs/>}/>
-          <Route exact path="/Admin/UpdateUser" element={<UpdateUser/>}/>
+          <Route exact path="/Admin/User/Admin/UpdateUser/:id" element={<UpdateUser/>}/>
 
           {/* Beneficiary */}
           <Route exact path="/Beneficiaries" element={<Beneficiaries />} />
